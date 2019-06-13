604ef18f5c4f63297ab389c3558618cd22280f46
Tobias Hjertelundh <tobias.hjertelundh@hotmail.com>
2019-06-13 14:23:04 +0200

    Implement navbar component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar'
 
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <div id="page-body">
         <div className="App">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
           <Route path="/articles-list" component={ArticlesList} />
-          <Route path="/article" component={ArticlePage} />
+          <Route path="/article/:name" component={ArticlePage} />
         </div>
       </div>
     </Router>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import articleContent from './Article-content';
 
-const ArticlePage = () => (
-    <>
-        <h1>This is an article</h1>
-    </>
-)
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name === name);
+
+    return (
+        <>
+        <h1>{article.title}</h1>
+        {article.content.map((paragraph, key) => (
+            <p key={key}>{paragraph}</p>
+        ))}
+        </>
+    );
+}
 
 export default ArticlePage;
