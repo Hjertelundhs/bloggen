9c77b9d4c7d1e2a21f841f5d64b31092357c6929
Tobias Hjertelundh <tobias.hjertelundh@hotmail.com>
2019-06-13 13:37:38 +0200

    Setup the main blog pages

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>About me!</h1>
+        <p>
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw 
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+        </p>
+        <p>
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw 
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+        </p>
+    </>
+)
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Hello and welcome to my Blog</h1>
+        <p>
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw 
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+        </p>
+        <p>
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw 
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+            Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw Fsdf sdsdf dsdfser grrwfeww fwewefw
+        </p>
+    </>
+)
+
+export default HomePage;
